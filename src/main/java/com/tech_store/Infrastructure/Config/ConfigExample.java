419a52558aca840eacaf5062c133c2c9b61bdf42
Richard Santos <richard.santos042525@gmail.com>
2025-09-24 13:40:21 -0400

    feature: estructura base del proyecto Closes #1

---
+++ src/main/java/com/tech_store/Infrastructure/Config/ConfigExample.java
@@ -0,0 +1,4 @@
+package com.tech_store.Infrastructure.Config;
+
+public class ConfigExample {
+}
---
+++ src/main/java/com/tech_store/Infrastructure/Controller/ControllerExample.java
@@ -0,0 +1,4 @@
+package com.tech_store.Infrastructure.Controller;
+
+public class ControllerExample {
+}
